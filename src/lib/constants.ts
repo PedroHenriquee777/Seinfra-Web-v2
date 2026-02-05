d0e93fa35467ad387d62ca9254c20fa4e153f233
PedroHenriquee777 <phznff13@gmail.com>
2026-02-05 08:29:47 -0300

    fix: api connection

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,41 +1,46 @@
-import { FileText, History, Phone } from "lucide-react";
+import { History } from "lucide-react";
+import { FileText } from "lucide-react";
+import { Phone } from "lucide-react";
 
 export const CATEGORIES = [
-	"Iluminação",
-	"Poda de árvore",
-	"Buraco na pista",
-	"Asfaltar",
-	"Outro",
+  "Iluminação",
+  "Pipa de Água",
+  "Coleta de Entulho de obra",
+  "Danificação da via",
+  "Poda de Árvore",
+  "Limpeza de Vegetação",
+  "Coleta de Resíduos Urbanos",
+  "Outro (Descreva o motivo na descrição)",
 ] as const;
 
-export function extrairCategoria(descricao: string): string {
-	const match = descricao.match(/^\[(.*?)\]/);
-	return match ? match[1] : "Outro";
+export function extractCategory(descricao: string): string {
+  const match = descricao.match(/^\[(.*?)\]/);
+  return match ? match[1] : "Outro";
 }
 
 export const navItems = [
-	{
-		Page: "/orderRegister",
-		Desc: "Informe uma nova Ordem de Serviço",
-		Icon: FileText,
-		Label: "Nova Ordem de Serviço",
-		className:
-			"aspect-[5/3] w-component-w sm:w-[40vw] lg:w-[30vw] h-48 lg:h-auto max-h-96",
-	},
-	{
-		Page: "/solicitacoes",
-		Desc: "Veja o andamento das suas Ordens de Serviço",
-		Icon: History,
-		Label: "Minhas Solicitações",
-		className:
-			"aspect-[5/3] w-component-w sm:w-[40vw] lg:w-[30vw] h-48 lg:h-auto max-h-96",
-	},
-	{
-		Page: "https://api.whatsapp.com/send?phone=558881677413",
-		Desc: "Envie mensagens diretamente para a Secretaria de infraestrutura",
-		Icon: Phone,
-		Label: "Mensagem Direta",
-		className:
-			"aspect-[5/3] w-component-w sm:w-[40vw] lg:w-[30vw] h-48 lg:h-auto max-h-96",
-	},
+  {
+    Page: "/orderRegister",
+    Desc: "Informe uma nova Ordem de Serviço",
+    Icon: FileText,
+    Label: "Nova Ordem de Serviço",
+    className:
+      "aspect-[5/3] w-component-w sm:w-[40vw] lg:w-[30vw] h-48 lg:h-auto max-h-96",
+  },
+  {
+    Page: "/request",
+    Desc: "Veja o andamento das suas Ordens de Serviço",
+    Icon: History,
+    Label: "Minhas Solicitações",
+    className:
+      "aspect-[5/3] w-component-w sm:w-[40vw] lg:w-[30vw] h-48 lg:h-auto max-h-96",
+  },
+  {
+    Page: "https://api.whatsapp.com/send?phone=558881677413",
+    Desc: "Envie mensagens diretamente para a Secretaria de infraestrutura",
+    Icon: Phone,
+    Label: "Mensagem Direta",
+    className:
+      "aspect-[5/3] w-component-w sm:w-[40vw] lg:w-[30vw] h-48 lg:h-auto max-h-96",
+  },
 ];
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { HomePage } from "../view/pages/Home/HomePage";
-import Cookie from "@/lib/cookie";
-import { redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    const login = Cookie.getCookie("login");
-    if (!login) {
-      throw redirect({ to: "/login" });
-    }
-  },
   component: HomePage,
 });
