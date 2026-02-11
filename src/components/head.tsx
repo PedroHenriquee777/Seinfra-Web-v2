d9a439fb09e944f44cb5fca50c99e4341dc57e37
PedroHenriquee777 <phznff13@gmail.com>
2026-02-11 08:40:24 -0300

    refactor: title modified to appear barely in responsiveness

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -3,46 +3,31 @@ import { ChevronLeftIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 interface HeaderProps {
-	Title: string;
-	position?: "fixed" | "relative" | "static";
+  position?: "fixed" | "relative" | "static";
 }
 
-export function Header({ Title, position = "fixed" }: HeaderProps) {
-	const isRelative = position === "relative";
+export function Header({ position = "fixed" }: HeaderProps) {
+  return (
+    <div
+      className={cn(
+        position === "fixed" && "fixed top-0",
+        position === "relative" && "relative",
+        position === "static" && "static",
+        "flex items-center w-full font-semibold text-[#3ab0b8] z-50",
+        "justify-center py-4",
+        "lg:justify-start lg:px-12"
+      )}
+    >
+      <Link
+        to="/"
+        className="absolute left-3 lg:static flex items-center"
+      >
+        <ChevronLeftIcon className="size-8 lg:size-10 text-seinfra-yellow-600" />
+      </Link>
 
-	
-	return (
-		<div
-			className={cn(
-				position === "fixed" && "fixed top-0",
-				position === "relative" && "relative",
-				position === "static" && "static",
-				"flex items-center w-full font-semibold text-[#3ab0b8] z-50",
-
-				!isRelative &&
-					"justify-center lg:justify-start lg:gap-12 pt-14 pb-4 lg:px-12",
-
-				isRelative && "justify-start sm:mt-4 gap-4 px-10 py-3 mt-12",
-			)}
-		>
-			<Link
-				to="/"
-				className={cn(
-					"flex items-center",
-					!isRelative && "absolute left-4 lg:static",
-				)}
-			>
-				<ChevronLeftIcon className="size-6 lg:size-10 text-seinfra-yellow-600" />
-			</Link>
-
-			<h1
-				className={cn(
-					"truncate",
-					isRelative ? "text-xl" : "text-2xl lg:text-3xl",
-				)}
-			>
-				{Title}
-			</h1>
-		</div>
-	);
+      <h1 className="text-xl lg:hidden">
+        Registrar Ordem
+      </h1>
+    </div>
+  );
 }
