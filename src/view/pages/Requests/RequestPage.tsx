c465f45f4d9a036508fd6a72e31e37d540b218f4
PedroHenriquee777 <phznff13@gmail.com>
2026-02-11 15:35:12 -0300

    fix: title fixed

--- src/view/pages/Requests/RequestPage.tsx
+++ src/view/pages/Requests/RequestPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Footer } from "@/components/footer";
-import { Header } from "@/components/head";
 import { RequestCard } from "@/components/request";
 import { cn } from "@/lib/utils";
 import { listRequests } from "@/services/request";
@@ -8,6 +7,8 @@ import type { ListRequestPayload } from "@/services/types/request-types";
 import { extractCategory } from "@/lib/constants";
 import { formatCPF } from "@/utils/format-cpf";
 import { LoaderDialog } from "@/components/loader-dialog";
+import { Link } from "@tanstack/react-router";
+import { ChevronLeftIcon } from "lucide-react";
 
 export function RequestPage() {
   const [orders, setOrders] = useState<ListRequestPayload[]>([]);
@@ -30,17 +31,34 @@ export function RequestPage() {
   }, []);
 
   return (
-    <div>
+    <div className="min-h-dvh flex flex-col">
       <LoaderDialog open={loading} />
-      <Header Title="Minhas Solicitações" />
+
+      <div
+        className={cn(
+          "fixed top-0",
+          "flex items-center w-full font-semibold text-[#3ab0b8] z-50",
+          "justify-center py-4",
+          "lg:justify-start lg:gap-12 pt-15 lg:pb-4 lg:px-12",
+          "bg-gray-100",
+        )}
+      >
+        <Link to="/" className="absolute left-3 lg:static flex items-center">
+          <ChevronLeftIcon className="size-8 lg:size-10 text-seinfra-yellow-600" />
+        </Link>
+
+        <h1 className="text-xl lg:text-3xl truncate">Minhas Solicitações</h1>
+      </div>
+
       <main
         className={cn(
           "bg-gray-100 text-seinfra-blue-light-400 font-semibold",
-          "flex flex-col items-center justify-center gap-8",
-          "min-h-dvh h-auto w-full overflow-hidden",
+          "flex flex-col items-center gap-8",
+          "w-full flex-1",
+          "pt-30 lg:pt-32",
         )}
       >
-        <div className="flex flex-col justify-center items-center gap-8 mt-28 mb-20 lg:mt-4">
+        <div className="flex flex-col justify-center items-center gap-8 mb-20">
           {!loading &&
             orders.map((order) => (
               <RequestCard
@@ -56,10 +74,10 @@ export function RequestPage() {
                 Name={order.applicant?.name ?? ""}
                 NumberHouse={order.applicant?.phone ?? ""}
                 CPF={order.applicant?.cpf ? formatCPF(order.applicant.cpf) : ""}
-                
               />
             ))}
         </div>
+
         <Footer />
       </main>
     </div>
